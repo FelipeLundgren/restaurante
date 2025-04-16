07f508bbf6e51ec7ea490d4a722c050997be1162
Felipe Lundgren <felipec5l2@hotmail.com>
2025-04-16 11:49:05 -0300

    adicionando ao git

---
+++ app.py
@@ -0,0 +1,16 @@
+from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+bebida_suco = Bebida('Suco de Melancia',5.0,'Grande')
+prato_paozinho = Prato('Paozinho', 2.0,'O melhor pao da cidade')
+
+
+
+def main():
+    print(bebida_suco)
+    print(prato_paozinho)
+
+if __name__ == '__main__':
+    main()
