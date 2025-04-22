14d000fcac46ae2407ab19b5c36bc30996c3a2ca
Felipe Lundgren <felipec5l2@hotmail.com>
2025-04-22 14:26:00 -0300

    adicionando desconto(polimorfirsmo

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia',5.0,'Grande')
 prato_paozinho = Prato('Paozinho', 2.0,'O melhor pao da cidade')
+bebida_suco.aplicar_desconto()
+prato_paozinho.aplicar_desconto()
+
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
